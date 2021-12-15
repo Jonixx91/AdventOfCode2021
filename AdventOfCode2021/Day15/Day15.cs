bb6dd8bf8d780b79a2896be2cf7d43d9daf16178
Jonas Lindemann <lindemann.jonas@gmail.com>
2021-12-15 14:29:40 +0100

    Day 15

---
+++ AdventOfCode2021/Day15/Day15.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using AdventOfCode2021.Day13.Folding;
+
+namespace AdventOfCode2021.Day15
+{
+    public class Day15 : Day
+    {
+        private int[,] _graph;
+        private int _maxX;
+        private int _maxY;
+
+        public Day15(bool isTest = false) 
+            : base(isTest)
+        {
+            _maxX = Input[0].Length;
+            _maxY = Input.Count;
+            _graph = new int[_maxX, _maxY];
+            for (int y = 0; y < _maxY; y++)
+                for (int x = 0; x < _maxX; x++)
+                    _graph[x, y] = int.Parse(new string(new[] { Input[y][x] }));
+        }
+
+        private int RunDijkstra()
+        {
+            var distance = new Dictionary<Point, int>();
+            var toVisit = new List<Point>();
+            for (int y = 0; y < _maxY; y++)
+            {
+                for (int x = 0; x < _maxX; x++)
+                {
+                    var p = new Point(x, y);
+                    distance[p] = Int32.MaxValue;
+                    toVisit.Add(p);
+                }
+            }
+
+            distance[new Point(0, 0)] = 0;
+            while (toVisit.Count > 0)
+            {
+                var closest = toVisit.Select(v => (Point: v, Distance: distance[v])).OrderBy(t => t.Distance).First().Point;
+                toVisit.Remove(closest);
+
+                foreach (var n in closest.GetNeighborgs(_maxX, _maxY))
+                {
+                    var newDist = distance[closest] + _graph[n.X, n.Y];
+                    if (newDist < distance[n])
+                        distance[n] = newDist;
+                }
+            }
+
+            return distance[new Point(_maxX - 1, _maxY - 1)];
+        }
+
+        public int Part1() => RunDijkstra();
+
+        private int ResetIfOver9(int i)
+        {
+            if (i > 9)
+                return i - 9;
+            return i;
+        }
+
+        private void EnlargeMap()
+        {
+            var newMap = new int[_maxY * 5, _maxY * 5];
+            for (int y = 0; y < _maxY; y++)
+                for (int x = 0; x < _maxX; x++)
+                    for (int i = 0; i <= 4; i++)
+                        newMap[x + i * _maxX, y] = ResetIfOver9(_graph[x, y] + i);
+            _maxX *= 5;
+
+            for (int y = 0; y < _maxY; y++)
+                for (int x = 0; x < _maxX; x++)
+                    for (int i = 0; i <= 4; i++)
+                        newMap[x, y + i * _maxY] = ResetIfOver9(newMap[x, y] + i);
+            _graph = newMap;
+            _maxY *= 5;
+        }
+
+        public int Part2()
+        {
+            EnlargeMap();
+            return RunDijkstra();
+        }
+    }
+}
---
+++ AdventOfCode2021/Day15/Point.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Day15
+{
+    public class Point : IEqualityComparer<Point>
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Point point &&
+                   X == point.X &&
+                   Y == point.Y;
+        }
+
+        public override int GetHashCode() => ToString().GetHashCode();
+
+        public bool Equals(Point x, Point y) => x.Equals(y);
+
+        public int GetHashCode(Point obj) => obj.GetHashCode();
+
+        private bool IsValid(int maxX, int maxY)
+        {
+            return X >= 0 &&
+                   X < maxX &&
+                   Y >= 0 &&
+                   Y < maxY;
+
+        }
+
+        public IEnumerable<Point> GetNeighborgs(int maxX, int maxY)
+        {
+            var ns = new Point[]
+            {
+                new Point(X - 1, Y),
+                new Point(X + 1, Y),
+                new Point(X, Y - 1),
+                new Point(X, Y + 1)
+            };
+            foreach (var n in ns)
+            {
+                if (n.IsValid(maxX, maxY))
+                    yield return n;
+            }
+        }
+
+        public override string ToString() => $"{X},{Y}";
+    }
+}
